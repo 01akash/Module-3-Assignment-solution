2b4dcbf111ad8513204cc62cd7dcd6da0a576d66
01akash <65606606+01akash@users.noreply.github.com>
2020-06-01 19:07:41 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+$(document).ready(function(){
+  $(".navTrigger").click(function(){
+    $("#menu").slideToggle();
+  });
+});
+
+
+//
+//$(".navTrigger").click(function(){
+//	$(".focus").toggle();
+//	});	
+//var flg = 0;
+//$(".navTrigger").click(function(){
+//	if(flg==0){
+//		$(this).addClass("open");
+//		flg=1;
+//		}	
+//else if(flg==1){
+//	$(this).removeClass("open");
+//	flg=0
+//	}
+//		
+//	
+//	})
+//
+//
+//
+
